2ab0c3647f71200251e477c2fe5b9a8c1fcb22e4
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-05-31 15:48:01 +0200

    added mORE

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var see = document.getElementById('see')
 var idInput = document.getElementById('projectid')
 see.addEventListener('submit', function(e){
     e.preventDefault()
-    document.location.href = './view.html?id='+e.target[0].value+'&title=my%20cool%20project&description=epic'
+    const id = e.target[0].value
+    
+    const title = e.target[1].value
+
+    const description = e.target[2].value
+
+    document.location.href = './view.html?id='+id+'&title='+title+'&description='+description
 })
